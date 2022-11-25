af38d5b06d33a10c9faab7958423f5a700c2888c
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-11-25 22:07:02 +0530

    misc: progress
    next:
    - gradient background for Sign In same can be utilize for create account
    - finish sign in page and wire back the sign in with github,google,twitter oauth

--- book-keeper/pages/index.tsx
+++ book-keeper/pages/index.tsx
@@ -1,55 +1,12 @@
 import type { NextPage } from 'next';
-import { trpcClient } from '../utils/Clientrpc';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import Landing from '../components/common/landing';
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  const {push} = useRouter();
-
-  const result = trpcClient.message.useQuery({name: '🐱‍🚀'});
-  console.log(session);
-
-  /**
-   * ⭐ redirect: false prevents page refresh on signout
-   * ⭐ also callbackUrl specify the page where to redirect the user after they signout change this to landing page url
-   */ 
-  const handleSignOut = async () => {
-    const data = await signOut({redirect: false, callbackUrl: '/landing'});
-    push(data.url)
-  }
-
-  const handleSignIn = () => signIn();
   
-  if(!result.data) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>  
-    )
-  }
   return (
-    <div>
-      <h1>{result.data.greeting}</h1>
-      <h4>{result.data.from}</h4>
-      {
-        session ? 
-          (
-            <>
-              <h3>You are signed in as {session.user?.email}</h3>
-              <Button onClick={handleSignOut}>SignOut</Button>
-            </>
-          )
-          : 
-          (
-            <>
-              <h3>You are not signed in</h3>
-              <Button onClick={handleSignIn}>SignIn</Button>
-            </>
-          )
-      }
-    </div>    
+    <>
+      <Landing/>
+    </>    
   )
 }
 
