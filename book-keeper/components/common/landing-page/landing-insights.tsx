cd6c328690c56a7e9551acfee1ede43d029b03ad
Jasmeet-1998 <devs.us.1984@gmail.com>
2023-01-20 22:09:22 +0530

    sorted: footer & feedback sections

--- book-keeper/components/common/landing-page/landing-insights.tsx
+++ book-keeper/components/common/landing-page/landing-insights.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import { motion } from 'framer-motion';
-import { LandingCustomTitleText, LandingCustomTypingText } from './landing-customs';
+import { LandingCustomTitleText, LandingCustomTypingText, InsightCard } from './landing-customs';
 import styles from '../../../styles/index';
-import { fadeIn, staggerContainer } from '../animations/landing_page.animations';
+import { staggerContainer } from '../animations/landing_page.animations';
+import { insightsList } from './landing-misc';
 
 
 const LandingInsights = () => (
-    <section>
+    <section className={`${styles.paddings} relative z-10`}>
+        <motion.div 
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={ {once: false,amount: 0.25} }
+            className={`${styles.innerWidth} mx-auto flex flex-col`}
+        >
+            <LandingCustomTypingText title="| Insight" textStyles="text-center" />
+            <LandingCustomTitleText title="Insight about Keeper." textStyles="text-center" />
+            <div className="mt-[50px] flex flex-col gap-[30px]">
+                {
+                    insightsList.map((insight,index)=>(
+                        <InsightCard key={`insight-${index}`} {...insight} index={index+1}/>
+                    ))
+                }
+            </div>
+        </motion.div>
     </section>           
 )
 
